fa72786a0cfadff6de0600697576430f7374fcfa
Romain Hernandez <romsher.dev@gmail.com>
2024-09-11 17:47:43 +0200

    fix: apos

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <div className="flex flex-col gap-4 text-white text-center p-4">
-      <h2>Vous n'avez plus aucune excuse pour ne pas connaître la météo du jour, même vous les geeks aux yeux fermés ! :)</h2>
-      <p>MétéoDou - Créé par <Link href="https://romain-hernandez.com" className="font-bold">Roms</Link></p>
+      <h2>Vous n&apos;avez plus aucune excuse pour ne pas connaître la météo du jour, même vous les geeks aux yeux fermés ! :)</h2>
+      <p>MétéoDou - Créé par <Link href="https://romain-hernandez.com" target="_blank" className="font-bold">Roms</Link></p>
     </div>
   )
 }
--- app/src/components/Main/Content-Presentation.jsx
+++ app/src/components/Main/Content-Presentation.jsx
@@ -2,7 +2,7 @@
 export default function ContentPresentation({ weatherData, localTime }) {
   return (
     <section className="flex sm:items-center sm:text-center flex-col gap-4 text-white">
-    <h1 className="text-2xl font-bold uppercase sm:text-center mb-4">Aujourd'hui à {weatherData.location.name} :</h1>
+    <h1 className="text-2xl font-bold uppercase sm:text-center mb-4">Aujourd&apos;hui à {weatherData.location.name} :</h1>
     <p >
       {weatherData.location.name} est une ville qui se trouve dans la
       région {weatherData.location.region} en{" "}
